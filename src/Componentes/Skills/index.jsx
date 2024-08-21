b8f76bee69305db0bc6ddccd0b8e1e96bdef8591
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 20:55:01 -0300

    vai palmeiras

--- src/Componentes/Skills/index.jsx
+++ src/Componentes/Skills/index.jsx
@@ -4,15 +4,15 @@ function Skills({ skills }) {
       <h2>Habilidades</h2>
                 <div class="skill-card">
                     <h3>HTML</h3>
-                    <p>Nível: Avançado</p>
+                    <p>Nível: ruim</p>
                 </div>
                 <div class="skill-card">
                     <h3>CSS</h3>
-                    <p>Nível: Intermediário</p>
+                    <p>Nível: pior ainda</p>
                 </div>
                 <div class="skill-card">
                     <h3>JavaScript</h3>
-                    <p>Nível: Básico</p>
+                    <p>Nível: meu deus</p>
                 </div>
     </div>
   );
